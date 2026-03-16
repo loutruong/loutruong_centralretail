8e60077e566e7a524b77d8995a2711f069e5037e
loutruong <thanh@loutruong.com>
2026-03-16 09:48:16 +0700

    Push

--- 04_plt/01_tracker_go.sql
+++ 04_plt/01_tracker_go.sql
@@ -38,7 +38,7 @@ WITH
 		WHERE
 			1 = 1 --
 			-- AND event_time >= CURRENT_DATE - INTERVAL '10 day'
-			AND (event_time BETWEEN '2026-01-28 00:00:00+07' AND '2026-01-30 23:59:59+07')
+			AND (event_time BETWEEN '2026-02-215 00:00:00+07' AND '2026-01-30 23:59:59+07')
 			-- AND event_time >= '2026-01-01 00:00:00+07'
 			AND LOWER(is_primary_attribution) = 'true'
 			AND LOWER(event_name) IN ('af_purchase_api', 'af_purchase')
