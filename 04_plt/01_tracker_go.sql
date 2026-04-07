77bc892351530c9752e2602942248674b1e4dff3
loutruong_window_crv <thanh@loutruong.com>
2026-04-07 09:39:04 +0700

    Push

--- 04_plt/01_tracker_go.sql
+++ 04_plt/01_tracker_go.sql
@@ -38,7 +38,7 @@ WITH
 		WHERE
 			1 = 1 --
 			-- AND event_time >= CURRENT_DATE - INTERVAL '10 day'
-			AND (event_time BETWEEN '2026-03-23 00:00:00+07' AND '2026-03-29 23:59:59+07')
+			AND (event_time BETWEEN '2026-03-01 00:00:00+07' AND '2026-03-31 23:59:59+07')
 			-- AND event_time >= '2026-01-01 00:00:00+07'
 			AND LOWER(is_primary_attribution) = 'true'
 			AND LOWER(event_name) IN ('af_purchase_api', 'af_purchase')
@@ -71,6 +71,6 @@ WITH
 SELECT
 	*
 FROM
-	t_dab
-	-- t_mab
+	-- t_dab
+	t_mab
 ;
