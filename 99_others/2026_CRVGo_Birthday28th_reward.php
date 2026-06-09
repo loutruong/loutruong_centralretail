0b5bfe14c3a833c1b5dd1b4d4168e9e5c2cc2709
loutruong_window_crv <thanh@loutruong.com>
2026-06-09 18:59:20 +0700

    Push

---
+++ 99_others/2026_CRVGo_Birthday28th_reward.php
@@ -0,0 +1,111 @@
+<?php
+
+$list = [
+    // hyper
+    "1" => [
+        'value' => 1000000,
+        'type' => 3,
+        'id' => 1,
+        'name' => '02 phiếu mua hàng/tháng, mỗi phiếu trị giá 500.000 VND',
+        'list' => []
+    ],
+    "2" => [
+        'value' => 500000,
+        'type' => 3,
+        'id' => 2,
+        'name' => '02 phiếu mua hàng/tháng, mỗi phiếu trị giá 250.000 VND',
+        'list' => []
+    ],
+    "3" => [
+        'value' => 300000,
+        'type' => 3,
+        'id' => 3,
+        'name' => '02 phiếu mua hàng/tháng, mỗi phiếu trị giá 150.000 VND',
+        'list' => []
+    ],
+    "4" => [
+        'value' => 100000,
+        'campaign' => 102882928,
+        'type' => 4,
+        'id' => 4,
+        'name' => 'Phiếu mua hàng dùng ngay trị giá 100.000 VND',
+        'list' => []
+    ],
+    "5" => [
+        'value' => 50000,
+        'campaign' => 102882929,
+        'type' => 4,
+        'id' => 5,
+        'name' => 'Phiếu mua hàng dùng ngay trị giá 50.000 VND',
+        'list' => []
+    ],
+    // tops
+    "6" => [
+        'value' => 500000,
+        'type' => 3,
+        'id' => 6,
+        'name' => '02 phiếu mua hàng/tháng, mỗi phiếu trị giá 250.000 VND',
+        'list' => []
+    ],
+    "7" => [
+        'value' => 300000,
+        'type' => 3,
+        'id' => 7,
+        'name' => '02 phiếu mua hàng/tháng, mỗi phiếu trị giá 150.000 VND',
+        'list' => []
+    ],
+    "8" => [
+        'value' => 100000,
+        'campaign' => 102882928,
+        'type' => 4,
+        'id' => 8,
+        'name' => 'Phiếu mua hàng dùng ngay trị giá 100.000 VND',
+        'list' => []
+    ],
+    "9" => [
+        'value' => 50000,
+        'campaign' => 102882929,
+        'type' => 4,
+        'id' => 9,
+        'name' => 'Phiếu mua hàng dùng ngay trị giá 50.000 VND',
+        'list' => []
+    ],
+    // mini go
+    "10" => [
+        'value' => 500000,
+        'type' => 3,
+        'id' => 10,
+        'name' => '02 phiếu mua hàng/tháng, mỗi phiếu trị giá 250.000 VND',
+        'list' => []
+    ],
+    "11" => [
+        'value' => 300000,
+        'type' => 3,
+        'id' => 11,
+        'name' => '01 phiếu mua hàng/tháng, mỗi phiếu trị giá 300.000 VND',
+        'list' => []
+    ],
+    "12" => [
+        'value' => 200000,
+        'type' => 3,
+        'id' => 12,
+        'name' => '01 phiếu mua hàng/tháng, mỗi phiếu trị giá 200.000 VND',
+        'list' => []
+    ],
+    "13" => [
+        'value' => 100000,
+        'campaign' => 102882928,
+        'type' => 4,
+        'id' => 13,
+        'name' => 'Phiếu mua hàng dùng ngay trị giá 50.000 VND',
+        'list' => []
+    ],
+    "14" => [
+        'value' => 20000,
+        'campaign' => 102882929,
+        'type' => 4,
+        'id' => 14,
+        'name' => 'Phiếu mua hàng dùng ngay trị giá 20.000 VND',
+        'list' => []
+    ],
+];
