0d1ae374b040325f37fb01bff46974a27a0a2de1
loutruong_window_crv <thanh@loutruong.com>
2026-06-24 17:03:25 +0700

    Push

--- 06_usr/search.sql
+++ 06_usr/search.sql
@@ -1,3 +1,43 @@
+SELECT
+	TO_CHAR(event_time, 'YYYY-MM')              AS mm,
+	(event_value::jsonb ->> 'af_search_string') AS search_term,
+	COUNT(*)                                    AS search_volume
+FROM
+	(
+		SELECT
+			*
+		FROM
+			bigc_tracking_db.bigc_tracking.in_app_event_non_organic_androids
+		UNION ALL
+		SELECT
+			*
+		FROM
+			bigc_tracking_db.bigc_tracking.in_app_event_non_organic_ios
+		UNION ALL
+		SELECT
+			*
+		FROM
+			bigc_tracking_db.bigc_tracking.in_app_event_organic_androids
+		UNION ALL
+		SELECT
+			*
+		FROM
+			bigc_tracking_db.bigc_tracking.in_app_event_organic_ios
+	) AS raw_events
+WHERE
+	1 = 1
+	AND (event_time BETWEEN '2026-02-01 00:00:00+07' AND '2026-03-01 00:00:00+07')
+	AND LOWER(is_primary_attribution) = 'true'
+	AND event_name = 'af_search'
+GROUP BY
+	1,
+	2 -- Grouping by the first and second columns (mm and search_term)
+HAVING
+	(event_value::jsonb ->> 'af_search_string') IS NOT NULL
+ORDER BY
+	search_volume DESC
+;
+
 --SELECT
 --	*
 --FROM
@@ -61,85 +101,86 @@
 --	1 = 1
 --	AND rn <= 50
 --;
-WITH
-	t_dwd AS (
-		SELECT
-			TO_CHAR(event_time, 'YYYY-MM') AS mm,
-			CASE
-				WHEN event_value IS JSON THEN (event_value::jsonb ->> 'af_search_string')
-				ELSE NULL
-			END AS search_term,
-			COUNT(*) AS search_volume
-		FROM
-			(
-				SELECT
-					*
-				FROM
-					bigc_tracking_db.bigc_tracking.in_app_event_non_organic_androids
-				UNION ALL
-				SELECT
-					*
-				FROM
-					bigc_tracking_db.bigc_tracking.in_app_event_non_organic_ios
-				UNION ALL
-				SELECT
-					*
-				FROM
-					bigc_tracking_db.bigc_tracking.in_app_event_organic_androids
-				UNION ALL
-				SELECT
-					*
-				FROM
-					bigc_tracking_db.bigc_tracking.in_app_event_organic_ios
-			)
-		WHERE
-			(event_time BETWEEN '2026-04-01 00:00:00+07' AND '2026-04-30 23:59:59+07')
-			AND LOWER(is_primary_attribution) = 'true'
-			AND LOWER(event_name) IN ('af_search')
-		GROUP BY
-			CASE
-				WHEN event_value IS JSON THEN (event_value::jsonb ->> 'af_search_string')
-				ELSE NULL
-			END,
-			TO_CHAR(event_time, 'YYYY-MM')
-	),
-	t_median AS (
-		SELECT
-			mm,
-			PERCENTILE_CONT(0.5) WITHIN GROUP (
-				ORDER BY
-					search_volume
-			) AS median_search_volume
-		FROM
-			t_dwd
-		GROUP BY
-			mm
-	)
-SELECT
-	t1.mm,
-	t1.search_term,
-	t1.search_volume,
-	t2.median_search_volume,
-	CASE
-		WHEN t1.search_volume >= t2.median_search_volume THEN 'Above Median'
-		ELSE 'Below Median'
-	END AS search_volume_label
-FROM
-	(
-		SELECT
-			*
-		FROM
-			t_dwd
-	) AS t1
-	LEFT JOIN (
-		SELECT
-			*
-		FROM
-			t_median
-	) AS t2 ON t1.mm = t2.mm
-WHERE
-	t1.search_term IS NOT NULL
-ORDER BY
-	t1.mm,
-	t1.search_volume DESC
-;
+-- WITH
+-- 	t_dwd AS (
+-- 		SELECT
+-- 			TO_CHAR(event_time, 'YYYY-MM') AS mm,
+-- 			CASE
+-- 				WHEN event_value IS JSON THEN (event_value::jsonb ->> 'af_search_string')
+-- 				ELSE NULL
+-- 			END AS search_term,
+-- 			COUNT(*) AS search_volume
+-- 		FROM
+-- 			(
+-- 				SELECT
+-- 					*
+-- 				FROM
+-- 					bigc_tracking_db.bigc_tracking.in_app_event_non_organic_androids
+-- 				UNION ALL
+-- 				SELECT
+-- 					*
+-- 				FROM
+-- 					bigc_tracking_db.bigc_tracking.in_app_event_non_organic_ios
+-- 				UNION ALL
+-- 				SELECT
+-- 					*
+-- 				FROM
+-- 					bigc_tracking_db.bigc_tracking.in_app_event_organic_androids
+-- 				UNION ALL
+-- 				SELECT
+-- 					*
+-- 				FROM
+-- 					bigc_tracking_db.bigc_tracking.in_app_event_organic_ios
+-- 			)
+-- 		WHERE
+-- 			(event_time BETWEEN '2026-04-01 00:00:00+07' AND '2026-04-30 23:59:59+07')
+-- 			AND LOWER(is_primary_attribution) = 'true'
+-- 			AND LOWER(event_name) IN ('af_search')
+-- 		GROUP BY
+-- 			CASE
+-- 				WHEN event_value IS JSON THEN (event_value::jsonb ->> 'af_search_string')
+-- 				ELSE NULL
+-- 			END,
+-- 			TO_CHAR(event_time, 'YYYY-MM')
+-- 	),
+-- 	t_median AS (
+-- 		SELECT
+-- 			mm,
+-- 			PERCENTILE_CONT(0.5) WITHIN GROUP (
+-- 				ORDER BY
+-- 					search_volume
+-- 			) AS median_search_volume
+-- 		FROM
+-- 			t_dwd
+-- 		GROUP BY
+-- 			mm
+-- 	)
+-- SELECT
+-- 	t1.mm,
+-- 	t1.search_term,
+-- 	t1.search_volume
+-- 	-- ,
+-- 	-- t2.median_search_volume,
+-- 	-- CASE
+-- 	-- 	WHEN t1.search_volume >= t2.median_search_volume THEN 'Above Median'
+-- 	-- 	ELSE 'Below Median'
+-- 	-- END AS search_volume_label
+-- FROM
+-- 	(
+-- 		SELECT
+-- 			*
+-- 		FROM
+-- 			t_dwd
+-- 	) AS t1
+-- 	LEFT JOIN (
+-- 		SELECT
+-- 			*
+-- 		FROM
+-- 			t_median
+-- 	) AS t2 ON t1.mm = t2.mm
+-- WHERE
+-- 	t1.search_term IS NOT NULL
+-- ORDER BY
+-- 	t1.mm,
+-- 	t1.search_volume DESC
+-- ;
